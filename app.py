228669899513e14788a89cd388a0b349d0621e84
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-03-26 18:54:46 +0200

    update the queing calculation , add server idleness percentage

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ waiting_time = []
 queue_list = []
 queue = 0
 queue_list_data = []
+idletime = 0
 print("Customer \t IAT \t ST \t Arrival Time \t Service Start \t Service End \t Waiting Time \t Queue \t")
 
 
-for i in range(7):
+for i in range(10):
     iat.append(r.randint(1, 5))
     st.append(r.randint(1, 7))
 
@@ -24,47 +25,50 @@ for i in range(7):
         waiting_time.append(service_start[i]-arrival_time[i])
         service_End.append(service_start[i]+st[i])
         queue_list.append(service_End[i])
-        queue=len(queue_list)-1
+        queue = len(queue_list)-1
         queue_list_data.append(queue)
+        idletime += service_start[i]
     else:
         arrival_time.append(arrival_time[i-1]+iat[i])
-      
+
         if arrival_time[i] < service_End[i-1]:
             service_start.append(service_End[i-1])
         else:
             service_start.append(arrival_time[i])
         service_End.append(service_start[i]+st[i])
+        idletime += service_start[i]-service_End[i-1]
         waiting_time.append(service_start[i]-arrival_time[i])
-        if( len(queue_list)>0):
-            
-            if(arrival_time[i]<queue_list[0]):
+        if(len(queue_list) > 0):
+
+            if(arrival_time[i] < queue_list[0]):
                 queue_list.append(service_End[i])
+                queue = len(queue_list)-1
                 queue_list_data.append(queue)
+                
 
             else:
-                queue_list.pop(0)
+                while(len(queue_list) > 0):
+                    if(arrival_time[i] >= queue_list[0]):
+                        queue_list.pop(0)
+                    else:
+                        break
                 queue_list.append(service_End[i])
+                queue = len(queue_list)-1
                 queue_list_data.append(queue)
-            if(len(queue_list)>0):
-                queue=len(queue_list)-1
-                queue_list_data.append(queue)
+                    
+            # if(len(queue_list) > 0):
+            #     queue = len(queue_list)-1
+            #     queue_list_data.append(queue)
         else:
             queue_list.append(0)
 
-        
-
-        
-
     print(f"{i+1} \t\t {iat[i]} \t {st[i]} \t\t {arrival_time[i]} \t\t {service_start[i]} \t\t {service_End[i]} \t\t {waiting_time[i]} \t {queue}\t\t")
-    
-    
+
 
 avg_waiting_time = np.mean(waiting_time)
 max_queue_length = max(queue_list_data)
-server_idle_time = service_start[0]/service_End[6]
+server_idle_time = idletime/service_End[-1]
 
 print(f"Average Waiting Time: {round(avg_waiting_time,2)} minutes")
 print(f"Maximum Queue Length: {max_queue_length} customers")
-print(f"Server Idle Time: {round(server_idle_time,2)} %")
-    
-  
+print(f"Server Idle Time: {round(server_idle_time,3)*100} %")
